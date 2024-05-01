b4a361fc800b4594f427fdbdd736bf7f1f7774b4
jg <iamjgl@naver.com>
2024-05-01 16:13:38 +0900

    리액트 초기설정 완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import NoName from './NoName';
 
 function App() {
+      //HTML과 다르다 = 구조만 유사 : 렌더링 과정에서 HTML으로 변환
+    //className 사용
+    // <>감싸주기
+    // <input/><br/> 무조건 닫아주기
+    //f1-terminal-npm start
+  
+    const $h2 = <h2>반가워~!~!</h2>;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    
+    <> 
+    <NoName />
+
+      <div className='App'>
+        <h1>메롱메롱~</h1>
+        {$h2}
+      </div>
+
+      <div className='noname'>
+        <input type='text'/>
+        <p>오늘은 5월 1일 행복한 수요일 입니다.<br/>
+        오후 3시 30분 입니다.<br/>
+        I LOVE REACT
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
+
   );
 }
 
---
+++ src/NoName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoName = () => {
+  return (
+    <div className='wrap'>
+      <h1>NoName;</h1>
+      </div>
+  )
+}
+
+export default NoName;
